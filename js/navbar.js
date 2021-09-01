7252622e3c267f3c85dc4208c94e1a4bbf330f61
harsha270501 <harshavardhinibalakrishnan@gmail.com>
2021-09-02 01:18:08 +0530

    completed payment history

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function changeFrame(i){
+    if(i==1){
+        document.getElementById("main-frame").src="dashboard.html";
+    }
+    else if(i==2){
+        document.getElementById("main-frame").src="feepaymentmain.html"
+    }
+}
