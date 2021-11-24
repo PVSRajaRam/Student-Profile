ac4a0cc18507f9e546828c434f8ea6cd8c0d1308
hiruthikj <hiruthik27@gmail.com>
2021-11-24 21:38:35 +0530

    Temp Push

--- Student-Profile-Servlet/src/CoursesAdmin.java
+++ Student-Profile-Servlet/src/CoursesAdmin.java
@@ -23,86 +23,88 @@ import models.Course;
 public class CoursesAdmin extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        Courses_DAO dao;
-//        RequestDispatcher dispatcher;
-//        dao = new Courses_DAO();
-//
-//        if (request.getParameter("achievement_to_del") != null) {
-//            try {
-//                deleteAchievementReq(request, response);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        } else if (request.getParameter("achievement_to_acc") != null) {
-//            try {
-//                acceptAchievement(request, response);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        } else if (request.getParameter("achievement_to_rej") != null) {
-//            try {
-//                rejectAchievement(request, response);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        } else {
-//            try {
-//                // HttpSession session=request.getSession(false);
-//                // String rollno=(String)session.getAttribute("rollno");
-//                // System.out.println(rollno);
-//
-//                List<Achievement> achievements = dao.getAllAchievements();
-//                request.setAttribute("achievements", achievements);
-//
-//                dispatcher = request.getRequestDispatcher("./jsp/achievements_admin.jsp");
-//                dispatcher.forward(request, response);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    private void acceptAchievement(HttpServletRequest request, HttpServletResponse response)
-//            throws IOException, SQLException {
-//        int achievement_id = Integer.parseInt(request.getParameter("achievement_to_acc"));
-//        Achievement_DAO dao = new Achievement_DAO();
-//
-//        dao.changeVerifiedStatus(achievement_id, 1);
-//        response.sendRedirect(getServletName());
-//
-//    }
-//
-//    private void rejectAchievement(HttpServletRequest request, HttpServletResponse response)
-//            throws IOException, SQLException {
-//        int achievement_id = Integer.parseInt(request.getParameter("achievement_to_rej"));
-//        Achievement_DAO dao = new Achievement_DAO();
-//
-//        dao.changeVerifiedStatus(achievement_id, -1);
-//        response.sendRedirect(getServletName());
-//
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        getAchievementData(request, response);
-//    }
-//
-//    private void deleteAchievementReq(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//        int achievement_id = Integer.parseInt(request.getParameter("achievement_to_del"));
-//        Achievement_DAO dao = new Achievement_DAO();
-//
-//        dao.deleteAchievement(achievement_id);
-//        response.sendRedirect(getServletName());
-//    }
-//
-//    private void getAchievementData(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Courses_DAO dao;
+        RequestDispatcher dispatcher;
+        dao = new Courses_DAO();
+
+        if (request.getParameter("course_to_del") != null) {
+            try {
+                deleteCourseReq(request, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (request.getParameter("course_to_acc") != null) {
+            try {
+                acceptCourse(request, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (request.getParameter("course_to_rej") != null) {
+            try {
+                rejectCourse(request, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+//                 HttpSession session=request.getSession(false);
+//                 String rollno=(String)session.getAttribute("rollno");
+//                 System.out.println(rollno);
+
+                List<Course> courses = dao.getAllCourses();
+                request.setAttribute("courses", courses);
+
+                dispatcher = request.getRequestDispatcher("./jsp/courses_admin.jsp");
+                dispatcher.forward(request, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void acceptCourse(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        int course_id = Integer.parseInt(request.getParameter("course_to_acc"));
+        String rollno = request.getParameter("rollno");
+        Courses_DAO dao = new Courses_DAO();
+
+        dao.changeVerifiedStatus(rollno, course_id, 1);
+        response.sendRedirect(getServletName());
+
+    }
+
+    private void rejectCourse(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+    	String rollno = request.getParameter("rollno");
+        int course_id = Integer.parseInt(request.getParameter("course_to_rej"));
+        Courses_DAO dao = new Courses_DAO();
+
+        dao.changeVerifiedStatus(rollno, course_id, -1);
+        response.sendRedirect(getServletName());
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    private void deleteCourseReq(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        int course_id = Integer.parseInt(request.getParameter("course_to_del"));
+        Courses_DAO dao = new Courses_DAO();
+
+        dao.deleteCourse(course_id);
+        response.sendRedirect(getServletName());
+    }
+
+//    private void getCourseData(HttpServletRequest request, HttpServletResponse response) {
 //        try {
-//            String title = request.getParameter("achievement_title");
-//            int achievement_type_id = Integer.parseInt(request.getParameter("achievement_type"));
+//            String title = request.getParameter("course_title");
+//            int course_type_id = Integer.parseInt(request.getParameter("course_type"));
 //
-//            String proof_date_str = request.getParameter("achievement_date");
+//            String proof_date_str = request.getParameter("course_date");
 //            SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yyyy", Locale.ENGLISH);
 //            Date proof_date = formatter.parse(proof_date_str);
 //            Date proof_date_sql = new java.sql.Date(proof_date.getTime());
@@ -110,12 +112,12 @@ public class CoursesAdmin extends HttpServlet {
 //            HttpSession session = request.getSession(false);
 //            String rollno = (String) session.getAttribute("rollno");
 //
-//            Achievement_DAO dao;
+//            Courses_DAO dao;
 //            RequestDispatcher dispatcher;
 //
-//            Achievement achv = new Achievement(rollno, achievement_type_id, title, proof_date_sql);
-//            dao = new Achievement_DAO();
-//            dao.addAchievement(achv);
+//            Course achv = new Course(rollno, course_type_id, title, proof_date_sql);
+//            dao = new Courses_DAO();
+//            dao.addCourse(achv);
 //
 //            doGet(request, response);
 //
